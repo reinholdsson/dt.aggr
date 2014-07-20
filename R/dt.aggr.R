40ca9352dab81298ca272e23ed02bd8ca42fd75a
Thomas Reinholdsson <thomas.reinholdsson@blocket.se>
2014-07-20 13:43:07 +0200

    Add data.table to depends

--- R/dt.aggr.R
+++ R/dt.aggr.R
@@ -1,16 +1,16 @@
 #' @export
-aggr_by <- function(data, j, by, grand.total = T, total.label = "(all)", envir = .GlobalEnv) {
+aggr_by <- function(dt, j, by, grand.total = T, total.label = "(all)", envir = .GlobalEnv) {
     j = substitute(j)
     
     # Calculate by each combination
     lst <- lapply(1:length(by), function(i) {
-      x <- data[, eval(j), by = eval(by[1:i])]
+      x <- dt[, eval(j), by = eval(by[1:i])]
       if (i != length(by)) x[, (by[-(1:i)]) := total.label]
       return(x)
     })
     
     # Grand total
-    if (grand.total) lst <- c(lst, list(data[, eval(j)][, (by) := total.label]))
+    if (grand.total) lst <- c(lst, list(dt[, eval(j)][, (by) := total.label]))
     
     # Combine all tables
     res <- rbindlist(lst, use.names = T, fill = F)
@@ -29,18 +29,17 @@ ytd_interval <- function(date, yt = 0) c(as.Date(paste0(year(date) + yt, "-01-01
 
 #' @export
 calculate_xtd <- function(x, date_col, last_date, fun = .N, fun.label = toupper(substitute(fun)), ...) {
-  
-  dt <- copy(x) # should at least select only relevant columns
+	dt <- copy(x) # should at least select only relevant columns
   setnames(dt, date_col, "..date..")
-		# Should use get (with envir) or quote/eval instead of setnames
-  	# But currently don't know how to solve this nicely.
+	# Should use get (with envir) or quote/eval instead of setnames
+  # But currently don't know how to solve this nicely.
   setkey(dt, ..date..)
   
 	d <- as.Date(last_date)
 	env <- environment()
 	fun <- substitute(fun)
   res <- aggr_by(dt,
-    list(
+    j = list(
       # Current year
       N_t1 = .SD[..date.. == get('d', envir), eval(get('fun', envir))],
       MTD_t1 = .SD[..date.. %between% mtd_interval(get('d', envir)), eval(get('fun', envir))],
--- README.md
+++ README.md
@@ -4,10 +4,14 @@ dt.aggr
 r data.table helper functions
 
 ```
+
 library(data.table)
 library(lubridate)
 d <- seq(as.Date("2013-01-01"), as.Date("2014-12-31"), by = 1)
 l <- length(d)
 x <- data.table(d = d, b = sample(LETTERS[1:3], l, replace = T), c = sample(LETTERS[1:3], l, replace = T), value = sample(1:100, l, replace = T))
-calculate_xtd(x, date_col = 'd', last_date = '2014-06-15', fun = sum(value), by = c('b'))
+calculate_xtd(x, date_col = 'd', last_date = as.Date('2014-06-15'), fun = sum(value), by = c('b'))
+
+
+
 ```
